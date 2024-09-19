4ff22b28afcc4688b13b9def9888a02465942503
MehmetOlgun <molgun-ext@appguard.us>
2024-09-19 20:38:23 +0300

    Added Query Anotation with Spring Data JPA.

--- src/main/java/com/example/repository/StudentRepository.java
+++ src/main/java/com/example/repository/StudentRepository.java
@@ -2,9 +2,23 @@ package com.example.repository;
 
 import com.example.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    //HQL : sınıfın ismi ve değişken isimleri kullanılarak sorgular yazılır.
+    @Query(value = "from Student" , nativeQuery = false)
+    List<Student> findAllStudent();
+
+     @Query(value = "from Student s WHERE s.id= :studentId")
+     Optional<Student> findStudentById(Integer studentId);
+
+    // SQL : tablo ismi ve tablo içerisindeki kolon isimleri ile sorgular yazılır.
+    @Query(value = "select * from student.student" , nativeQuery = true)
+    List<Student> findAllStudents();
 }

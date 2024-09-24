60df155f566dfe7865ecd00c0776306d99427dd0
MehmetOlgun <molgun-ext@appguard.us>
2024-09-24 15:51:56 +0300

    Added OneToMany relation example.

---
+++ src/main/java/com/example/entities/Employee.java
@@ -0,0 +1,25 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    private Department department;
+
+}

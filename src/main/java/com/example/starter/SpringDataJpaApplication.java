ff675896bca90497af6b5903d7dd1fbf518a267d
MehmetOlgun <molgun-ext@appguard.us>
2024-09-28 14:15:56 +0300

    Add detailed notes on @ConfigurationProperties usage in Spring Boot
    - Overview of @ConfigurationProperties annotation.
    - How to use and configure custom properties.
    - Example of binding configuration properties to a class.
    - Explanation of prefix usage and validation.
    
    This commit improves the documentation for better understanding of application configuration.

--- src/main/java/com/example/starter/SpringDataJpaApplication.java
+++ src/main/java/com/example/starter/SpringDataJpaApplication.java
@@ -1,8 +1,10 @@
 package com.example.starter;
 
+import com.example.configuration.GlobalProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.example"})
 @ComponentScan(basePackages = {"com.example"})
 @EnableJpaRepositories(basePackages = {"com.example"})
+@EnableConfigurationProperties(value = GlobalProperties.class)
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
